f8ad20aa77f52984565b77b142b956ceeaa18a67
Erich <erichroeseler123@gmail.com>
2026-02-23 06:56:58 -0700

    Add venue slugs

--- lib/venues.ts
+++ lib/venues.ts
@@ -1,6 +1,8 @@
 export const VENUE_SLUGS = [
   "red-rocks-amphitheatre",
+  "mission-ballroom",
+  "fiddlers-green-amphitheatre",
+  "fillmore-auditorium",
+  "gothic-theatre",
   "mishawaka-amphitheatre",
 ] as const;
-
-export type VenueSlug = (typeof VENUE_SLUGS)[number];
